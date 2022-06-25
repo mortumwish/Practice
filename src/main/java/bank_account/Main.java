3bb5ee6661e361bbcb1b1f009b55cc93fcccef08
Nikita) <chissen@mail.ru>
2022-06-25 20:25:18 +0300

    Доработаны sout'ы.

--- src/main/java/bank_account/Main.java
+++ src/main/java/bank_account/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("All users:");
         UserAccount nikita = new UserAccount("Nikita", "+79631233172", 10000);
         UserAccount danil1 = new UserAccount("Danil", "+79178996899", 8000);
-        UserAccount danil2 = new UserAccount("Danil", "+79378893213", 5000);
+        UserAccount danil2 = new UserAccount("Danil", "+79378893213", 5000); // нужен для демонстрации работы выбора по имени
 
         //Перевод денежных средств и последующая информация об аккаунтах пользователей:
         nikita.transfer(danil1, 3500); //Никита дал в долг Данилу 3500
@@ -14,14 +14,10 @@ public class Main {
 
         System.out.println("///");
 
-        //Попытка поиска аккаунтов по различным критериям:
-        UserAccount.findUser(0).infoOfUser();
-        UserAccount.findUser("Danil").infoOfUser();
-        UserAccount.findUser("Danil").transfer(UserAccount.findUser("Nikita"), 3500);
-
-                                                                                             //После запроса ввести айди,
-                                                                                             // находим по балансу того Данила, кто у нас занимал
-                                                                                             // 3500. Данил вернул долг Никите)
+        //Поиск аккаунтов по различным критериям и перевод долга в размере 3500 обратно Никите:
+        UserAccount.findUser("Danil").transfer(UserAccount.findUser(0), 3500);
+        // После запроса ввести айди, находим по балансу того Данила, кто у нас занимал 3500, вводим его айди, т.е.
+        // единицу, в консоль. Данил вернул долг Никите)
         nikita.infoOfUser();
         danil1.infoOfUser();
     }
--- src/main/java/bank_account/UserAccount.java
+++ src/main/java/bank_account/UserAccount.java
@@ -19,15 +19,16 @@ public class UserAccount {
         this.userId = numberOfUsers;
         users.add(this);
         numberOfUsers++;
-        System.out.println(infoOfUser(this));
+        infoOfUser();
     }
 
     public void infoOfUser() {
-        System.out.println("Info of User: \n" + infoOfUser(this));
+        System.out.println("Info of User: \n  Id: " + this.userId + "; Name: " + this.userName + "; Phone number: " + this.userPhone + "; Deposit: " + this.deposit + ".");
     }
 
     public void transfer(UserAccount transferTo, int sumOfMoneyToTransfer) {
-        System.out.println("Command: Attempting to transfer money from account ID: " + this.userId + " to account ID: " + transferTo.userId);
+        System.out.println("Command: Attempting to transfer money from account ID: " + this.userId + " (" + this.userName + ")"
+                                                                + " to account ID: " + transferTo.userId + " (" + transferTo.userName + ")");
         if (this.deposit <= 0 || sumOfMoneyToTransfer > this.deposit) {
             System.out.println("Error. Not enough money for a transfer.");
         } else {
@@ -37,14 +38,10 @@ public class UserAccount {
         }
     }
 
-    public static String infoOfUser(UserAccount user) {
-        return "  Id: " + user.userId + "; Name: " + user.userName + "; Phone number: " + user.userPhone + "; Deposit: " + user.deposit + ".";
-    }
-
     public static UserAccount findUser(int userId) {
         try {
             System.out.println("Command: Trying to find user by userID: " + userId);
-            System.out.println("User found:\n" + infoOfUser(users.get(userId)));
+            System.out.println("User found successfully");
             return users.get(userId);
         } catch (IndexOutOfBoundsException exception) {
             System.out.println("There is no user with this id");
@@ -64,26 +61,22 @@ public class UserAccount {
             System.out.println("No Matches");
             return null;
         } else if (foundList.size() == 1) {
-            System.out.println("1 search match by \"" + userName + "\": \n" + infoOfUser(foundList.get(0)));
+            System.out.println("1 search match by \"" + userName + "\". The search was successful.");
             return foundList.get(0);
         } else {
             System.out.println("Error. Several matches were found. Information about all matches is displayed. Please find a user by his ID.");
             for (UserAccount user : foundList) {
-                System.out.println(infoOfUser(user));
+                System.out.println("  Id: " + user.userId + "; Name: " + user.userName + "; Phone number: " + user.userPhone + "; Deposit: " + user.deposit + ".");
             }
             Scanner in = new Scanner(System.in);
             while (true) {
                 try {
                     System.out.print("Input a userID: ");
-                    int selectedID = in.nextInt();
-                    System.out.println("Selected user:\n" + infoOfUser(users.get(selectedID)));
-                    return users.get(selectedID);
+                    return users.get(in.nextInt());
                 } catch (IndexOutOfBoundsException exception) {
                     System.out.println("Choose an id from the list!");
                 }
             }
         }
     }
-
-
 }
