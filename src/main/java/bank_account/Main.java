2ea54bb857b6461d8c54cad18461a6e0b784d659
Nikita) <chissen@mail.ru>
2022-06-25 22:25:00 +0300

    Доработка классов и методов

--- src/main/java/bank_account/Main.java
+++ src/main/java/bank_account/Main.java
@@ -2,23 +2,10 @@ package main.java.bank_account;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("All users:");
         UserAccount nikita = new UserAccount("Nikita", "+79532435543", 10000);
         UserAccount danil1 = new UserAccount("Danil", "+788005553535", 8000);
         UserAccount danil2 = new UserAccount("Danil", "+79378893213", 5000); // нужен для демонстрации работы выбора по имени
 
-        //Перевод денежных средств и последующая информация об аккаунтах пользователей:
-        nikita.transfer(danil1, 3500); //Никита дал в долг Данилу 3500
-        nikita.infoOfUser();
-        danil1.infoOfUser();
-
-        System.out.println("///");
-
-        //Поиск аккаунтов по различным критериям и перевод долга в размере 3500 обратно Никите:
-        UserAccount.findUser("Danil").transfer(UserAccount.findUser(0), 3500);
-        // После запроса ввести айди, находим по балансу того Данила, кто у нас занимал 3500, вводим его айди, т.е.
-        // единицу, в консоль. Данил вернул долг Никите)
-        nikita.infoOfUser();
-        danil1.infoOfUser();
+        UserAccount.findUsersByName("Danil").transfer(UserAccount.findUserById(0), 3500);
     }
 }
--- src/main/java/bank_account/UserAccount.java
+++ src/main/java/bank_account/UserAccount.java
@@ -1,59 +1,30 @@
 package main.java.bank_account;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class UserAccount {
-    public String userName;
-    public String userPhone;
-    public int userId;
-    public int deposit;
-    public static int numberOfUsers = 0;
-    public static ArrayList<UserAccount> users = new ArrayList<>();
+class Bank {
+    protected static List<UserAccount> users = new ArrayList<>();
 
-    UserAccount(String userName, String userPhone, int deposit) {
-        this.userName = userName;
-        this.userPhone = userPhone;
-        this.deposit = deposit;
-        this.userId = numberOfUsers;
-        users.add(this);
-        numberOfUsers++;
-        infoOfUser();
-    }
-
-    public void infoOfUser() {
-        System.out.println("Info of User: \n  Id: " + this.userId + "; Name: " + this.userName + "; Phone number: " + this.userPhone + "; Deposit: " + this.deposit + ".");
-    }
-
-    public void transfer(UserAccount transferTo, int sumOfMoneyToTransfer) {
-        System.out.println("Command: Attempting to transfer money from account ID: " + this.userId + " (" + this.userName + ")"
-                                                                + " to account ID: " + transferTo.userId + " (" + transferTo.userName + ")");
-        if (this.deposit <= 0 || sumOfMoneyToTransfer > this.deposit) {
-            System.out.println("Error. Not enough money for a transfer.");
-        } else {
-            transferTo.deposit = transferTo.deposit + sumOfMoneyToTransfer;
-            this.deposit = this.deposit - sumOfMoneyToTransfer;
-            System.out.println("The transfer was successful.");
-        }
-    }
-
-    public static UserAccount findUser(int userId) {
+    public static UserAccount findUserById(int userId) {
         try {
             System.out.println("Command: Trying to find user by userID: " + userId);
+            UserAccount foundUser = users.get(userId);
             System.out.println("User found successfully");
-            return users.get(userId);
+            return foundUser;
         } catch (IndexOutOfBoundsException exception) {
             System.out.println("There is no user with this id");
             return null;
         }
     }
 
-    public static UserAccount findUser(String userName) {
+    public static UserAccount findUsersByName(String userName) {
         System.out.println("Command: Searching user by " + "\"" + userName + "\"");
         ArrayList<UserAccount> foundList = new ArrayList<>();
         for (UserAccount currentUser : users) {
-            if (Objects.equals(currentUser.userName, userName)) {
+            if (Objects.equals(currentUser.name, userName)) {
                 foundList.add(currentUser);
             }
         }
@@ -65,18 +36,54 @@ public class UserAccount {
             return foundList.get(0);
         } else {
             System.out.println("Error. Several matches were found. Information about all matches is displayed. Please find a user by his ID.");
-            for (UserAccount user : foundList) {
-                System.out.println("  Id: " + user.userId + "; Name: " + user.userName + "; Phone number: " + user.userPhone + "; Deposit: " + user.deposit + ".");
+            return getUserByIdFromScanner(foundList);
+        }
+    }
+
+    public static UserAccount getUserByIdFromScanner(ArrayList<UserAccount> foundList) {
+        for (UserAccount user : foundList) {
+            System.out.println("  Id: " + user.id + "; Name: " + user.name + "; Phone number: " + user.phone + "; Deposit: " + user.deposit + ".");
+        }
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print("Input a userID: ");
+                return users.get(in.nextInt());
+            } catch (IndexOutOfBoundsException exception) {
+                System.out.println("Choose an id from the list!");
             }
-            Scanner in = new Scanner(System.in);
-            while (true) {
-                try {
-                    System.out.print("Input a userID: ");
-                    return users.get(in.nextInt());
-                } catch (IndexOutOfBoundsException exception) {
-                    System.out.println("Choose an id from the list!");
-                }
-            }
+        }
+    }
+}
+
+
+class UserAccount extends Bank {
+    int id;
+    String phone;
+    String name;
+    int deposit;
+
+    UserAccount(String userName, String userPhone, int deposit) {
+        this.id = users.size();
+        this.phone = userPhone;
+        this.name = userName;
+        this.deposit = deposit;
+        users.add(this);
+    }
+
+    public void showUserInfo() {
+        System.out.println("Info of User: \n  Id: " + this.id + "; Name: " + this.name + "; Phone number: " + this.phone + "; Deposit: " + this.deposit + ".");
+    }
+
+    public void transfer(UserAccount transferTo, int sumOfMoneyToTransfer) {
+        System.out.println("Command: Attempting to transfer money from account ID: " + this.id + " (" + this.name + ")"
+                + " to account ID: " + transferTo.id + " (" + transferTo.name + ")");
+        if (this.deposit <= 0 || sumOfMoneyToTransfer > this.deposit) {
+            System.out.println("Error. Not enough money for a transfer.");
+        } else {
+            transferTo.deposit += sumOfMoneyToTransfer;
+            this.deposit -= sumOfMoneyToTransfer;
+            System.out.println("The transfer was successful.");
         }
     }
 }
